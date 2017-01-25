7486120fd2848a0dcf87df428f0db4bac2e42f70
Leland Krych <leland.krych@gmail.com>
2017-01-25 20:57:04 +0000

    Add files to week5

---
+++ week5scc/strongly_connected.rb
@@ -0,0 +1,16 @@
+def scc(graph)
+end
+
+def load_graph(file)
+end
+
+def dfs(graph,start_vertex)
+    explored = [start_vertex]
+    for edges in vertex
+        if edge is unexplored?
+            dfs
+        end
+    end
+end
+
+    
