c6abd2f8a4a7b0be7c81862d2627b5deb9f648ec
Leland Krych <leland.krych@gmail.com>
2017-02-02 22:25:06 +0000

    Completed strongly-connected components algorithm and spec

--- week5scc/spec/strongly_connected_spec.rb
+++ week5scc/spec/strongly_connected_spec.rb
@@ -1,2 +1,15 @@
 require_relative 'spec_helper.rb'
 require_relative '../strongly_connected.rb'
+
+graph1 = {"A" => ["D","C"], "B" => ["A"], "C" => ["B","H","I"], "D"=> ["E","G"], "E" => ["F"], "F" => ["G"], "G" => ["E"], "H" => ["J", "K"], "I" => ["H"], "J" => ["K"], "K" => ["I"]}
+graph2 = {"1" => ["4"], "2" => ["8"], "3" => ["6"], "4" => ["7"], "5" => ["2"], "6" => ["9"], "7" => ["1"], "8" => ["6", "5"], "9" => ["7", "3"]}
+
+describe "test scc functionality" do
+  it 'performs scc correctly' do
+    expect(scc(graph1)).to eq(4)
+  end
+  
+  it 'performs scc correctly' do
+    expect(scc(graph2)).to eq(3)
+  end
+end
--- week5scc/strongly_connected.rb
+++ week5scc/strongly_connected.rb
@@ -1,16 +1,51 @@
 def scc(graph)
-end
-
-def load_graph(file)
-end
-
-def dfs(graph,start_vertex)
-    explored = [start_vertex]
-    for edges in vertex
-        if edge is unexplored?
-            dfs
+    #use kosaraju's algorithm with two pass dfs to compute strongly connected components
+    #FIRST reverse edges of graph and find order of node discovery
+    explored = []
+    order_nodes = []
+    components = []
+    rev_graph = reverse(graph)
+    graph.keys.reverse.each do |node|
+        unless explored.include?(node)
+            dfs(rev_graph, node, explored, order_nodes)
         end
     end
+    
+    explored = [] #reset explored
+    #Go through the graph on a second pass, this time iterate through the reverse order of the nodes found in the first DFS
+    scc = 0
+    order_nodes.reverse.each do |node|
+        unless explored.include?(node)
+            scc += 1
+            dfs(graph, node, explored, components)
+        end
+    end
+    return scc #returns number of components
+    
 end
 
+
+def dfs(graph,start,explored,bookkeeping)
+    explored.push(start)
+    graph[start].shuffle.each do |node|
+      unless explored.include?(node)
+          dfs(graph,node,explored, bookkeeping)
+      end
+    end
+    bookkeeping.push(start)
+end
+
+def reverse(g)
+ #g is an adjacency list form of graph
+ rev_graph = Hash.new {|h,k| h[k] = [] }
+ g.keys.each do |node|
+     g[node].each do |edge|
+         rev_graph[edge] << node
+     end
+     if rev_graph[node].nil?
+       rev_graph[node] = []
+     end
+ end
+ return rev_graph
+end
     
