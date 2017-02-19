afa649d214e0dbe0f6fc707df851dd986f1098e1
Leland Krych <leland.krych@gmail.com>
2017-02-19 23:18:50 +0000

    rejigger test for two_sum benchmarking

--- week8hash/benchmarking/two_sum.rb
+++ week8hash/benchmarking/two_sum.rb
@@ -4,17 +4,17 @@ require_relative '../two_sum_bloom.rb'
 require_relative '../two_sum_hash.rb'
 require_relative '../two_sum_naive.rb'
 
-data = Array.new(100000) { rand(-10000...10000) }
+data = Array (1..10000)
 
 Benchmark.bm do |b|
 	b.report("Naive two-sum implementation: ") do
-	    naive_two_sum(150,data)
+	    naive_two_sum(19999,data)
 	end
 	b.report("Hashtable two-sum implementation: ") do
-	    hash_two_sum(150,data)
+	    hash_two_sum(19999,data)
 	end
 	b.report("Bloom Filter two-sum implementation: ") do
-	    bloom_two_sum(150,data)
+	    bloom_two_sum(19999,data)
 	end
 end
 	    
